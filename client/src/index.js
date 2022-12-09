f94a1ee5d50e3d19cf5a908323c7a412b8d05695
kchen19 <97988297+kchen19@users.noreply.github.com>
2022-12-09 03:01:00 -0500

    added separate pages

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,18 @@ import {
 	Switch
 } from 'react-router-dom';
 
+import HomePage from './pages/HomePage';
+import PlayersPage from './pages/PlayersPage';
+import 'antd/dist/antd.css';
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from './pages/Home';
+import "shards-ui/dist/css/shards.min.css"
+import MatchesPage from './pages/MatchesPage';
+
+
+import PlaylistPage from './pages/PlaylistPage';
+import IndivSongPage from './pages/IndivSongPage';
+import ExploreSongsPage from './pages/ExploreSongsPage';
 
 ReactDOM.render(
   <div>
@@ -16,7 +26,36 @@ ReactDOM.render(
         <Route exact
 							path="/"
 							render={() => (
-								<Home />
+								<HomePage />
+							)}/>
+        <Route exact
+							path="/players"
+							render={() => (
+								<PlayersPage />
+							)}/>
+        <Route exact
+							path="/matches"
+							render={() => (
+								<MatchesPage />
+							)}/>
+
+
+		<Route exact
+							path="/playlist"
+							render={() => (
+								<PlaylistPage />
+							)}/>
+
+		<Route exact
+							path="/indivSong"
+							render={() => (
+								<IndivSongPage />
+							)}/>
+
+		<Route exact
+							path="/explore"
+							render={() => (
+								<ExploreSongsPage />
 							)}/>
       </Switch>
     </Router>

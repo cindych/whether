22def73ce76cebb4c7d1ff5c424cc795338a2e01
Cindy Chen <cindych@sas.upenn.edu>
2022-12-15 21:42:20 -0500

    added descr/format to cities, explore, playlist

--- client/src/pages/explore/ExplorePage.js
+++ client/src/pages/explore/ExplorePage.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
 
 import SongsForAttributeWeatherComponent from './SongsForAttributeWeatherComponent.js';
 import SongsForDateRegionComponent from './SongsForDateRegionComponent.js';
@@ -30,17 +32,29 @@ export default function ExplorePage() {
                     justifyContent: 'space-between',
                     width: '50%',
                 }}>
-                    <SongsForRegionWeatherComponent setIsLoading={setIsLoading}/>
-                    <SongsForDateRegionComponent setIsLoading={setIsLoading}/>
+                    <Paper elevation={2} sx={{ padding: '2%', marginRight: '3%' }}>
+                        <Typography sx={{ display: 'block', textAlign: 'center', marginBottom: '2%' }} variant="caption">find songs based on weather & region</Typography>
+                        <SongsForRegionWeatherComponent setIsLoading={setIsLoading}/>
+                    </Paper>
+                    <Paper elevation={2} sx={{ padding: '2%', marginTop: '3%', marginRight: '3%' }}>
+                        <Typography sx={{ display: 'block', textAlign: 'center', marginBottom: '2%' }} variant="caption">find songs based on region & date</Typography>
+                        <SongsForDateRegionComponent setIsLoading={setIsLoading}/>
+                    </Paper>
                 </Box>
                 <Box sx={{
                     display: 'flex',
                     flexDirection: 'column',
-                    justifyContent: 'space-between',
+                    justifyContent: 'flex-start',
                     width: '50%',
                 }}>
-                    <SongsForAttributeWeatherComponent setIsLoading={setIsLoading}/>
-                    <SongStatsComponent isLoading={isLoading} setIsLoading={setIsLoading}/>
+                    <Paper elevation={2} sx={{ padding: '2%' }}>
+                        <Typography sx={{ display: 'block', textAlign: 'center', marginBottom: '2%' }} variant="caption">find songs based on weather & attribute (default: all songs)</Typography>
+                        <SongsForAttributeWeatherComponent setIsLoading={setIsLoading}/>
+                    </Paper>
+                    <Paper elevation={2} sx={{ padding: '2%', marginTop: '3%' }}>
+                        <Typography sx={{ display: 'block', textAlign: 'center', marginBottom: '2%' }} variant="caption">find attribute stats for songs played on given weather & region</Typography>
+                        <SongStatsComponent isLoading={isLoading} setIsLoading={setIsLoading}/>
+                    </Paper>
                 </Box>
             </Box>
         </Container>

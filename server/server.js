30896995c37a0dbd1c62fdf569ff3a8c079f3078
Anna Xia <44536369+annaanx24@users.noreply.github.com>
2022-12-16 18:23:56 -0500

    change url

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // whitelist localhost 3000
-app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
+app.use(cors({ credentials: true, origin: ['https://whether.herokuapp.com/'] }));
 
 // return all songs
 app.get('/allSongs', routes.allSongs)
